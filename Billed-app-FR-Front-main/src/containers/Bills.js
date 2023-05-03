cf57d8d1abe2ce83053c8a2fa0911818560765f8
JaRoD41 <laurent.trinco@gmail.com>
2023-05-03 12:19:26 +0200

    1st test OK

--- Billed-app-FR-Front-main/src/containers/Bills.js
+++ Billed-app-FR-Front-main/src/containers/Bills.js
@@ -43,7 +43,6 @@ export default class {
 						.sort((a, b) => new Date(b.date) - new Date(a.date))
 						.map((doc) => {
 							try {
-								console.log('doc :', doc)
 								return {
 									...doc,
 									date: formatDate(doc.date),
