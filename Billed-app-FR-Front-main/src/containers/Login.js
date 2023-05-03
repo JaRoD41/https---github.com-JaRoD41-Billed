a82992af3c4b7710fd960394e4a62d88bbd0d834
JaRoD41 <laurent.trinco@gmail.com>
2023-05-03 12:45:39 +0200

    add comment

--- Billed-app-FR-Front-main/src/containers/Login.js
+++ Billed-app-FR-Front-main/src/containers/Login.js
@@ -41,6 +41,7 @@ export default class Login {
     e.preventDefault()
     const user = {
       type: "Admin",
+      // Je corrige le bug de l'admin qui ne peut pas se connecter en modifiant les champs email et password
       email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
       password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
